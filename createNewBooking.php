17cfaaf383f918e97e758f957bb00e4c65350b73
shero121 <keithsherwin6764@gmail.com>
2023-10-18 09:30:46 +0100

    finished part 1 and understand why it wont add above 13 and 15

--- createNewBooking.php
+++ createNewBooking.php
@@ -1,5 +1,7 @@
 <?php 
 
+
+
 $mid = $_POST['memberID']; 
 $cid = $_POST['courtID']; 
 $bd = $_POST['bookingDate']; 
@@ -8,16 +10,21 @@ $et = $_POST['endTime'];
 
 include("dbcon.php");
 
+
+
 $sql = "insert into booking(memberid,courtid,bookingDate,starttime,endtime) values ('$mid','$cid','$bd','$st','$et')";
 
-echo $sql;
 /*
+echo $sql;
+*/
+
+
 if (mysqli_query($conn, $sql)) {
   echo "<br>New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-*/
+
 
 mysqli_close($conn); 
 ?>
